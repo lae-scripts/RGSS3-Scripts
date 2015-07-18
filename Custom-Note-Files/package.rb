344d3d4dc8bcf6fae68223804abc2d9e4278ac5c
lae-scripts <lanthanumentertainment@hotmail.com>
2015-07-18 21:12:23 +0800

    Create package.rb

---
+++ Custom-Note-Files/package.rb
@@ -0,0 +1,8 @@
+# Package description
+  Package.new(
+  	name: 'Custom Note Files',
+  	version: vsn(1, 1, 0),
+  	authors: {'Adiktuzmiko' => 'adiktuzmiko@hotmail.com'},
+  	components: ['LAE_CNF_Script.rb'],
+  	description: 'Provides a method of loading notes from external sources',
+  )
